58f82248c0e81bb8b6d5fdaea319445a30fd3a45
thilo.bangert <thilo.bangert@8b64e154-0f13-11de-9106-f38757f67524>
2010-08-25 14:13:50 +0000

    fix compiler warnings due to incorrect encoding

--- src/org/zoolu/tools/Base64.java
+++ src/org/zoolu/tools/Base64.java
@@ -123,9 +123,9 @@ public class Base64 {
 		}
 
 		if (i == iter) {
-			// per come è costruita la codifica a base64
+			// per come e costruita la codifica a base64
 			// togliendo gli eventuali caratteri "=" di padding
-			// il resto può essere 0, 2 o 3
+			// il resto puo essere 0, 2 o 3
 			if (resto == 0) {
 			}
 			if (resto == 2) {
@@ -166,6 +166,6 @@ public class Base64 {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("messaggio decodificato è: " + strdecodificato);
+		System.out.println("messaggio decodificato e: " + strdecodificato);
 	}
 }
