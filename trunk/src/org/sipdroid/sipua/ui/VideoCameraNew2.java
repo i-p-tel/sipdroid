269715799eef29ef859ec98b8359a61f5656b5b9
viktor.pathi@gmail.com <viktor.pathi@gmail.com@8b64e154-0f13-11de-9106-f38757f67524>
2013-10-23 11:28:14 +0000

    fix Lint errors

--- trunk/src/org/sipdroid/sipua/ui/VideoCameraNew2.java
+++ trunk/src/org/sipdroid/sipua/ui/VideoCameraNew2.java
@@ -23,8 +23,11 @@ package org.sipdroid.sipua.ui;
 
 import java.io.IOException;
 
+import android.annotation.TargetApi;
 import android.hardware.Camera;
+import android.os.Build;
 
+@TargetApi(Build.VERSION_CODES.FROYO) 
 public class VideoCameraNew2 {
 	static void reconnect(Camera c) {
 		try {
